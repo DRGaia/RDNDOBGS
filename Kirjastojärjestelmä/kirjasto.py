75e5d9f4f1a8bf756c11a2c9a0e28c53abf3732a
Daniel Rittinghaus <daniel.rittinghaus@kukkula.local>
2026-04-08 15:34:48 +0300

    tallennus

--- Kirjastojärjestelmä/kirjasto.py
+++ Kirjastojärjestelmä/kirjasto.py
@@ -1,6 +1,7 @@
-import mysql.connector
+import mysql.connector, sqlite3
 from tabulate import tabulate
-
+conn = sqlite3.connect('./Kannat/Kirjasto.db')
+cur = conn.cursor()
 mydb = mysql.connector.connect(
   host="localhost",
   port="3306",
@@ -15,13 +16,31 @@ try:
   try:
     if valinta == "1":
       kumpi = input("\nOletko:\n\n1) Admin.\n\n2) Asiakas.\n")
-      if kumpi == 1:
+      if kumpi == "1":
         try:
           email = input("Anna sähköpostiosoitteesi: ")
-            
           salasana = input("Anna salasanasi: ")
+          cur.execute("SELECT nimi FROM admin WHERE sähköpostiosoite = ? AND salasana = ? LIMIT 1", (email, salasana))
+          onko = cur.fetchone()
+          if onko:
+            print(f"Tervetuloa {onko[0]}")
+          else:
+            print("Väärä salasana tai sähköposti")
         except:
-          pass
+          print("error")
+
+    if kumpi == "2":
+        try:
+          email = input("Anna sähköpostiosoitteesi: ")
+          salasana = input("Anna salasanasi: ")
+          cur.execute("SELECT nimi FROM admin WHERE sähköpostiosoite = ? AND salasana = ? LIMIT 1", (email, salasana))
+          onko = cur.fetchone()
+          if onko:
+            print(f"Tervetuloa {onko[0]}")
+          else:
+            print("Väärä salasana tai sähköposti")
+        except:
+          print("error")
   except:
     pass
 except:
