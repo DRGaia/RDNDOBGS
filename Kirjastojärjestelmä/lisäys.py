e13cc30ebe27bf020eec8a9ac509c3abba3e504f
OlaviLaakkonen <ol4660@edu.turku.fi>
2026-04-08 15:48:10 +0300

    Add files via upload

---
+++ Kirjastojärjestelmä/lisäys.py
@@ -0,0 +1,60 @@
+import sqlite3
+import mysql.connector
+from tabulate import tabulate
+
+conn = sqlite3.connect('./Kannat/Kirjasto.db')
+cur = conn.cursor()
+
+valinta = input("Haluatko lisätä (k)irjan vai (a)siakkaan: ").lower()
+
+if valinta == "k":
+            print("")
+            liskirnimi = input('Anna lisättävän kirjan nimi: ')
+
+            print("")
+
+            liskirkirj = input(f'Anna kirjan "{liskirnimi}" kirjoittaja: ')
+
+            print("")
+
+            liskirvuos = int(input(f'Anna kirjan "{liskirnimi}" julkaisuvuosi: '))
+
+            print("")
+
+            liskirisbn = int(input(f'Anna kirjan "{liskirnimi}" ISBN-13: '))
+
+            print("")
+
+            cur.execute('INSERT INTO kirjat (nimi, kirjoittaja, julkaisuvuosi, ISBN) VALUES (?, ?, ?, ?)', (liskirnimi, liskirkirj, liskirvuos, liskirisbn))
+            cur.execute('SELECT * FROM kirjat')
+            data = cur.fetchall()
+            table = tabulate(data)
+            conn.commit()
+            print(table)
+
+if valinta == "a":
+            print("")
+            lisasinimi = input('Anna lisättävän asiakkaan nimi: ')
+
+            print("")
+
+            lisasivuos = int(input(f'Anna asiakkaan "{lisasinimi}" syntymävuosi: '))
+
+            print("")
+
+            lisasisähkö = input(f'Anna asiakkaan "{lisasinimi}" sähköposti: ')
+
+            print("")
+
+            lisasisaldo = int(input(f'Anna asiakkaan "{lisasinimi}" sakkosaldo: '))
+
+            print("")
+
+            cur.execute('INSERT INTO asiakkaat (nimi, syntymävuosi, sähköpostiosoite, sakkosaldo) VALUES (?, ?, ?, ?)', (lisasinimi, lisasivuos, lisasisähkö, lisasisaldo))
+            cur.execute('SELECT * FROM asiakkaat')
+            data = cur.fetchall()
+            table = tabulate(data)
+            conn.commit()
+            print(table)
+else:
+            print("Kirjoita k tai a! ")
---
+++ Kirjastojärjestelmä/poisto.py
@@ -0,0 +1,43 @@
+import sqlite3
+import mysql.connector
+from tabulate import tabulate
+
+conn = sqlite3.connect('./Kannat/Kirjasto.db')
+cur = conn.cursor()
+
+poistovalinta = input("Haluatko poistaa (k)irjan vai (a)siakkaan: ").lower()
+
+if poistovalinta == "a":
+            print("")
+            cur.execute('SELECT * FROM asiakkaat')
+            data = cur.fetchall()
+            table = tabulate(data)
+            print(table)
+            print("")
+            poistomail = input('Anna poistettavan asiakkaan sähköpostiosoite: ')
+            print("")
+            cur.execute('DELETE FROM asiakkaat WHERE sähköpostiosoite = ?', (poistomail,))
+            conn.commit()
+            cur.execute('SELECT * FROM asiakkaat')
+            data = cur.fetchall()
+            table = tabulate(data)
+            print(table)
+
+elif poistovalinta == "k":
+            print("")
+            cur.execute('SELECT * FROM kirjat')
+            data = cur.fetchall()
+            table = tabulate(data)
+            print(table)
+            print("")
+            poistoisbn = int(input('Anna poistettavan kirjan ISBN: '))
+            print("")
+            cur.execute('DELETE FROM kirjat WHERE ISBN = ?', (poistoisbn,))
+            conn.commit()
+            cur.execute('SELECT * FROM kirjat')
+            data = cur.fetchall()
+            table = tabulate(data)
+            print(table)
+
+else:
+            print("Kirjoita joko a tai k! ")
