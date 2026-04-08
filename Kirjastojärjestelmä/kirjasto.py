882100c08ddfeeed30589c9d374fe9174ec61fa3
Daniel Rittinghaus <daniel.rittinghaus@kukkula.local>
2026-04-08 14:02:32 +0300

    py

--- Kirjastojärjestelmä/kirjasto.py
+++ Kirjastojärjestelmä/kirjasto.py
@@ -9,3 +9,20 @@ mydb = mysql.connector.connect(
   use_pure=True
 )
 
+print("Tervetuloa \n\n1) Kirjaudu sisään\n\n2) Luo käyttäjä\n")
+valinta = input()
+try:
+  try:
+    if valinta == "1":
+      kumpi = input("\nOletko:\n\n1) Admin.\n\n2) Asiakas.\n")
+      if kumpi == 1:
+        try:
+          email = input("Anna sähköpostiosoitteesi: ")
+            
+          salasana = input("Anna salasanasi: ")
+        except:
+          pass
+  except:
+    pass
+except:
+  pass
