ef12c137032f296f11ee992b4519b409ce70ef6d
Olavi Laakkonen <olavi.laakkonen@kukkula.local>
2026-04-14 10:24:22 +0300

    haku ja jotai shittii

---
+++ Kirjastojärjestelmä/haku.py
@@ -0,0 +1,20 @@
+import sqlite3
+import mysql.connector
+from tabulate import tabulate
+
+conn = sqlite3.connect('./Kannat/Kirjasto.db')
+cur = conn.cursor()
+
+Ekirja = input("Mitä kirjaa etsit (kirjoita kirjan nimi tai kirjailija): ")
+
+Tulokset = cur.execute("SELECT * FROM kirjat WHERE nimi LIKE ? OR kirjoittaja LIKE ?",('%' + Ekirja + '%', '%' + Ekirja + '%')).fetchall()
+
+if not Tulokset:
+    print("Kirjaa ei löytynyt.")
+    
+elif len(Ekirja) < 6:
+    print("Kirjoita kokonimi. ")
+
+else:
+    for kirja in Tulokset:
+        print(kirja)
