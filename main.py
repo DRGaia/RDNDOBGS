cd9375b29587fcab63dfab03f7704e04b8d97af7
Daniel Rittinghaus <daniel.rittinghaus@kukkula.local>
2026-04-22 12:31:17 +0300

    Tarkistus

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from Kirjastojärjestelmä import kirjautuminen, lisäys, poisto, haku;
+from Kirjastojärjestelmä import kirjautuminen, lisäys, poisto, haku, tarkistus;
 while True:
     kirjautuminen.logsignin()
     admin = kirjautuminen.admin
     asiakas = kirjautuminen.asiakas
+    käyt = kirjautuminen.käyt
     if admin == True:
         while True:
-            print("\nMitä haluat tehdä? \nAdmin: \na1) Kirjan lisäys\n\na2) Kirjan poistaminen\n\n1) Kirjan hakeminen\n")
+            print("\nMitä haluat tehdä? \nAdmin: \na1) Kirjan lisäys\n\na2) Kirjan poistaminen\n\n1) Kirjan hakeminen\n2) Käyttäjätiedot\n")
             mikä = input()
             if mikä == "a1":
                 lisäys.lisäys()
@@ -13,8 +14,13 @@ while True:
                 poisto.poisto()
             elif mikä == "1":
                 haku.haku()
+            elif mikä == "2":
+                tarkistus.tarkistus(admin,asiakas,käyt)
     elif asiakas == True:
-        print("\n1) Kirjan hakeminen\n")
-        mikä = input()
-        if mikä == "1":
-                haku.haku()
+        while True:
+            print("\n1) Kirjan hakeminen\n2) Käyttäjätiedot\n")
+            mikä = input()
+            if mikä == "1":
+                    haku.haku()
+            elif mikä == "2":
+                tarkistus.tarkistus(admin,asiakas,käyt)
